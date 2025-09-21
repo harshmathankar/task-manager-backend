d954f052d35d3534c2605dc0e19d3c3c00daba55
harshmathankar <mathankarharsh31@gmail.com>
2025-09-21 19:20:59 +0530

    removed console message

--- src/config/db.js
+++ src/config/db.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Database connection error:', error.message);
     process.exit(1);
